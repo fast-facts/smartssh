b2d968d09b70bc48902de3c966d536e7e8424081
RabbitPlayground <107660271+RabbitPlayground@users.noreply.github.com>
2022-06-10 15:24:47 -0400

    Formatting fixes

--- adapters/configs/ftp-simple-config.js
+++ adapters/configs/ftp-simple-config.js
@@ -8,46 +8,46 @@ function Crypto() {
 }
 
 Crypto.prototype =
-	{
-		encrypt: function (a, b, c) {
-			if (!b) b = this.decodeHex(this.KEY);
-			if (!c) c = this.decodeHex(this.IV);
-			var d = crypto.createCipheriv('aes-128-cbc', new Buffer(b, 'hex'), new Buffer(c, 'hex'));
-			return d.update(a, 'utf8', 'hex') + d.final('hex')
-		}
-		, decrypt: function (a, b, c) {
-			if (!b) b = this.decodeHex(this.KEY);
-			if (!c) c = this.decodeHex(this.IV);
-			var d = crypto.createDecipheriv('aes-128-cbc', new Buffer(b, 'hex'), new Buffer(c, 'hex'));
-			return d.update(a, 'hex', 'utf8') + d.final('utf8')
-		}
-		, hashing: function (a, b) {
-			if (!b) b = "sha256";
-			var c = crypto.createHash(b);
-			c.update(a);
-			return c.digest('hex')
-		}
-		, encodeBase64: function (a, b) {
-			if (!b) b = "utf8";
-			return new Buffer(a, b).toString('base64')
-		}
-		, decodeBase64: function (a, b) {
-			if (!b) b = "utf8";
-			return new Buffer(a, 'base64').toString(b)
-		}
-		, encodeHex: function (a, b) {
-			if (!b) b = "utf8";
-			return new Buffer(a, b).toString('hex')
-		}
-		, decodeHex: function (a, b) {
-			if (!b) b = "utf8";
-			return new Buffer(a, 'hex').toString(b)
-		}
-	};
+{
+	encrypt: function (a, b, c) {
+		if (!b) b = this.decodeHex(this.KEY);
+		if (!c) c = this.decodeHex(this.IV);
+		var d = crypto.createCipheriv('aes-128-cbc', new Buffer(b, 'hex'), new Buffer(c, 'hex'));
+		return d.update(a, 'utf8', 'hex') + d.final('hex')
+	}
+	, decrypt: function (a, b, c) {
+		if (!b) b = this.decodeHex(this.KEY);
+		if (!c) c = this.decodeHex(this.IV);
+		var d = crypto.createDecipheriv('aes-128-cbc', new Buffer(b, 'hex'), new Buffer(c, 'hex'));
+		return d.update(a, 'hex', 'utf8') + d.final('utf8')
+	}
+	, hashing: function (a, b) {
+		if (!b) b = "sha256";
+		var c = crypto.createHash(b);
+		c.update(a);
+		return c.digest('hex')
+	}
+	, encodeBase64: function (a, b) {
+		if (!b) b = "utf8";
+		return new Buffer(a, b).toString('base64')
+	}
+	, decodeBase64: function (a, b) {
+		if (!b) b = "utf8";
+		return new Buffer(a, 'base64').toString(b)
+	}
+	, encodeHex: function (a, b) {
+		if (!b) b = "utf8";
+		return new Buffer(a, b).toString('hex')
+	}
+	, decodeHex: function (a, b) {
+		if (!b) b = "utf8";
+		return new Buffer(a, 'hex').toString(b)
+	}
+};
 
 adapter.filename = 'ftp-simple.json';
 adapter.codesettings = false;
-adapter.formatter = function(content) {
+adapter.formatter = function (content) {
 	var c = new Crypto();
 	var json = c.decrypt(content);
 	var result = true;
